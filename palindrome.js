5a05b24720f597691fdc4c1bc4cd66d66c08e704
Yana Matsakova <60586314+YanaMatsakova@users.noreply.github.com>
2023-01-28 14:47:40 +0300

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,34 @@
+// шалаш
+// Вася
+// А муза рада музе без ума да разума
+// "Пустите!" - Летит супу миска Максиму. - "Пустите, летит суп!"
+// 123321
+
+function palindrome(str) {
+  str = str
+    .toString()
+    .toLowerCase()
+    .replace(/\s|[,.!?"/-]/g, "");
+  return str === str.split("").reverse().join("");
+}
+
+function superPalindrome(str) {
+  const halfOfLength = Math.floor(str.length / 2);
+  str = str.toLowerCase().replace(/\s|[,.!?"/-`':;]/g, "");
+  for (let i = 0; i < halfOfLength; i++)
+    if (str[i] !== str[str.length - i - 1]) {
+      return false;
+    }
+  return true;
+}
+
+console.log(palindrome("шалаш"));
+console.log(palindrome("Вася"));
+console.log(palindrome("А муза рада музе без ума да разума"));
+console.log(
+  palindrome('"Пустите!" - Летит супу миска Максиму. - "Пустите, летит суп!"')
+);
+console.log(palindrome(123321));
+
+// Тут много палиндромов
+// http://www.perevod71.ru/palindromy-predlozhenija
